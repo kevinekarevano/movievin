a33a0f62b032d2d07ef7878cd9415c04ba217f27
kevinekarevano <kev1n.ek4revano@gmail.com>
2024-08-20 18:43:09 +0700

    fix gambar 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,9 +5,9 @@ const ErrorPage = () => {
   const error = useRouteError();
   return (
     <div className="bg-primaryBg min-h-screen gap-10 flex flex-col justify-center items-center">
-      <img className=" fixed left-0" src="" alt="assets/404.svg" />
-      <img className="fixed w-72 top-0 right-0" src="src/assets/blob2.png" alt="" />
-      <img className="w-1/2" src="src/assets/404.svg" alt="" />
+      <img className=" fixed left-0" src="assets/blob1.png" alt="404 IMAGE" />
+      <img className="fixed w-72 top-0 right-0" src="assets/blob2.png" alt="" />
+      <img className="w-1/2" src="assets/404.svg" alt="" />
       <div className="bg-gradient-to-r from-[#F78FF8] via-[#A5A586] to-[#844BD2] bg-clip-text">
         <p className="text-transparent text-xl font-bold ">{error.statusText || "Not Found"}.</p>
       </div>
