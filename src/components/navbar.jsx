1c9ea151c7d1fc11360567261422bd947619909f
kevinekarevano <kev1n.ek4revano@gmail.com>
2024-08-22 12:49:40 +0700

    Mengubah ui Hero Section dan navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,12 @@
-const Navbar = () => {
+import Search from "./search";
+
+const Navbar = ({ searchHandle, searchRef }) => {
   return (
-    <nav className="fixed w-full  z-[99999] ">
-      <div className="bg-[#151515]  text-cyan-900 p-3">
-        <div className="w-40 p-2">
-          <img src="assets/logo.svg" alt="Logo" />
+    <nav className="  ">
+      <div className="bg-[#151515]  flex justify-between items-center  text-cyan-900 p-5">
+        <img className=" w-40 h-full " src="assets/logo.svg" alt="Logo" />
+        <div className=" ">
+          <Search change={searchHandle} inputRef={searchRef} />
         </div>
       </div>
     </nav>
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,11 +1,20 @@
 import { useState, useEffect, useRef } from "react";
 import Navbar from "@/components/navbar";
 import MovieCard from "@/components/fragments/movieCard";
-import Search from "@/components/search";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import Loaders from "@/components/loaders";
 import { Separator } from "@/components/ui/separator";
+import { Swiper, SwiperSlide } from "swiper/react";
+import Dot from "@/components/dot";
 
+import { FiArrowUpRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 const HomePage = () => {
   const [discoverMovies, setDiscoverMovies] = useState([]);
@@ -46,6 +55,7 @@ const HomePage = () => {
         if (!res.ok) throw new Error("Failed fetch discover movies ");
 
         const data = await res.json();
+        console.log(data);
         setDiscoverMovies(data.results);
       } catch (err) {
         setError(err.message);
@@ -132,15 +142,50 @@ const HomePage = () => {
 
   return (
     <div className="bg-primaryBg min-h-screen">
-      <Navbar />
+      <Navbar searchHandle={handleChange} searchRef={searchInputRef} />
       {/* <img className="fixed rotate-180 w-80 right-0" src="src/assets/blob1.png" alt="" />
       <img className="fixed rotate-180 top-10 left-0" src="src/assets/blob2.png" alt="" /> */}
-      <div className="flex pt-32 flex-col justify-center items-center min-h-[50vh] mb-20">
-        <div className="justify-center flex mt-10">
-          <img className="w-[32rem]" src="assets/logo.svg" alt="logo" />
-        </div>
 
-        <Search change={handleChange} inputRef={searchInputRef} />
+      <div className="bg-red-200 min-h-screen overflow-hidden">
+        <Swiper
+          spaceBetween={0}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper "
+        >
+          {discoverMovies.map((item) => (
+            <SwiperSlide key={item.id} style={{ backgroundImage: `url('https://image.tmdb.org/t/p/original${item.backdrop_path}')` }} className=" bg-cover ">
+              <div className="bg-black h-screen p-32 bg-opacity-70 w-full ">
+                <div className="w-1/2 ">
+                  <p className="font-bold text-5xl mb-5   text-white ">{item.title}</p>
+                  <div className="flex items-center gap-2 mb-4">
+                    <p className="text-white font-semibold">{item.release_date ? item.release_date.split("-")[0] : "N/A"}</p>
+                    <Dot />
+                    <p className="text-yellow-300  bg-primaryBg px-2 rounded-md font-medium ">{item.vote_average ? item.vote_average.toFixed(1) : "N/A"}</p>
+                    <Dot />
+                    <p className="text-white bg-primaryBg px-2  rounded-md font-medium"> {item.original_language.toUpperCase()}</p>
+                  </div>
+                  <p className="text-white">{item.overview}</p>
+
+                  <Link to={`/movie/${item.id}`}>
+                    <div className="mt-6 hover:w-1/3 hover:tracking-widest transition-all duration-500 hover:bg-zinc-700 hover:bg-opacity-55  bg-primaryBg border-zinc-700 border-2 bg-opacity-75 w-1/4 justify-center gap-2 cursor-pointer rounded-full h-11  flex items-center">
+                      <p className="font-bold text-white">Detail</p>
+                      <FiArrowUpRight color="white" size={20} />
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
 
       <div className="">
@@ -157,7 +202,7 @@ const HomePage = () => {
 
       <div className="flex">
         <ScrollArea className="w-1/6 h-screen p-5">
-          <p className="text-white font-monument  text-3xl">Genres</p>
+          <p className="text-white font-monument  text-2xl">Genres</p>
           {genres.length > 0 ? (
             <div>
               {genres.map((item, index) => (
