a94d94acf48405ee7f7421bcbfd87215f785e027
kevinekarevano <kev1n.ek4revano@gmail.com>
2024-09-10 09:45:29 +0700

    changes navbar color

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,13 +5,12 @@ const Navbar = ({ searchHandle, searchRef, isShow, type = false }) => {
     <>
       {type ? (
         <nav className={`${isShow ? "top-5" : "-top-24"} duration-500 transition-all fixed z-[99999] flex w-full justify-center`}>
-          <Search change={searchHandle} inputRef={searchRef}  />
+          <Search change={searchHandle} inputRef={searchRef} />
         </nav>
       ) : (
         <nav>
-          <div className={` bg-cover bg-center  bg-[url('https://i.pinimg.com/originals/01/9f/5c/019f5c50d2c49ae35aaefc9834bf4550.gif')]  flex justify-between items-center  text-cyan-900 p-5`}>
+          <div className={` bg-cover bg-center bg-gradient-to-r from-primaryBg to-slate-900  flex justify-between items-center  text-cyan-900 p-5`}>
             <img className=" w-40 h-full " src="assets/logo.svg" alt="Logo" />
-            
           </div>
         </nav>
       )}
